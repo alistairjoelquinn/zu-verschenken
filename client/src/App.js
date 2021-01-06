d1466d9aa598fedc81c5cf6f0e0b6bdddc6065f7
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-01-06 20:21:30 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import 'normalize.css';
+
+const AppStyles = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: dodgerblue;
+    color: antiquewhite;
+    div {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 40px;
+        font-weight: bold;
+        -webkit-text-stroke: 1px tomato;
+    }
+`;
+
+export default function App() {
+    return (
+        <AppStyles>
+            <div>React App / Styled Components / Node Server</div>
+        </AppStyles>
+    );
+}
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = () => ({
+    entry: [
+        "@babel/polyfill",
+        path.join(__dirname, "client", "src", "start.js"),
+    ],
+    output: {
+        path: path.join(__dirname, "client", "public"),
+        filename: "bundle.js",
+    },
+    performance: {
+        hints: false,
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "client", "public"),
+        proxy: {
+            "/": {
+                target: "http://localhost:3001",
+            }
+        },
+        port: "3000",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+            }
+        ],
+    }
+});
