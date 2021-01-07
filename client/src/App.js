8173d6318e9e5c226db8ffeb1bd7c3a9d986554a
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-01-07 21:51:46 +0100

    marker functionality added so users can click map to mark point

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import { formatRelative } from 'date-fns';
 import 'normalize.css';
 import '@reach/combobox/styles.css';
+import styled from 'styled-components';
+import { v4 } from 'uuid';
 
 import mapStyles from './mapStyles';
+
+const HeaderStyles = styled.h1`
+    position: absolute;
+    top: 0;
+    left: 0; 
+    z-index: 2;
+    background-color: transparent;
+    font-family: Arial, Helvetica, sans-serif;
+    padding-left: 20px;
+`;
 
 const libraries = ['places'];
 const mapContainerStyle = {
@@ -25,6 +38,7 @@ export default function App() {
         googleMapsApiKey: apiKey,
         libraries
     });
+    const [giftMarkers, setGiftMarkers] = useState([]);
 
     if (loadError) {
         return 'Error loading!';
@@ -34,13 +48,27 @@ export default function App() {
     }
     return (
         <div>
+            <HeaderStyles>Zu Verschenken 🎁</HeaderStyles>
             <GoogleMap
                 mapContainerStyle={mapContainerStyle}
                 zoom={12}
                 center={center}
                 options={options}
+                onClick={e => setGiftMarkers(val => [...val, {
+                    lat: e.latLng.lat(),
+                    lng: e.latLng.lng(),
+                    time: new Date()
+                }])}
             >
-
+                {giftMarkers.map(item => (
+                    <Marker
+                        key={v4()}
+                        position={{
+                            lat: item.lat,
+                            lng: item.lng
+                        }}
+                    />
+                ))}
             </GoogleMap>
         </div>
     );
