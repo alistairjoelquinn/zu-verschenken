d0067f9d945299c06a74b29cb2383f36bd563d6e
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-01-07 21:24:09 +0100

    map loads on home page focussing on berlin

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,47 @@
 import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import { formatRelative } from 'date-fns';
+import 'normalize.css';
+import '@reach/combobox/styles.css';
+
+import mapStyles from './mapStyles';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh'
+};
+const center = {
+    lat: 52.520008,
+    lng: 13.404954
+};
+const options = {
+    styles: mapStyles,
+    disableDefaultUI: true,
+    zoomControl: true
+};
 
 export default function App() {
-    console.log('apiKey: ', apiKey);
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: apiKey,
+        libraries
+    });
 
+    if (loadError) {
+        return 'Error loading!';
+    }
+    if (!isLoaded) {
+        return 'Loading Maps...';
+    }
     return (
-        <div>Google Maps React Components</div>
+        <div>
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={12}
+                center={center}
+                options={options}
+            >
+
+            </GoogleMap>
+        </div>
     );
 }
