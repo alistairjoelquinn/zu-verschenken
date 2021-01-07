210f2cfddbc05653862c3e9158066e557e824aaf
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-01-07 16:51:17 +0100

    packages updated to include google maps / env variables used to store keys

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,8 @@
-import styled from 'styled-components';
-import 'normalize.css';
-
-const AppStyles = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin: 0;
-    height: 100vh;
-    width: 100vw;
-    background-color: dodgerblue;
-    color: antiquewhite;
-    div {
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 40px;
-        font-weight: bold;
-        -webkit-text-stroke: 1px tomato;
-    }
-`;
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import { formatRelative } from 'date-fns';
 
 export default function App() {
     return (
-        <AppStyles>
-            <div>Google Maps React Components</div>
-        </AppStyles>
+        <div>Google Maps React Components</div>
     );
 }
