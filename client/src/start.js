72b18806e32e54d660eb09a4635d3b1c8186aebd
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-01-13 14:57:02 +0100

    redux set up for handling data in state

--- client/src/start.js
+++ client/src/start.js
@@ -1,3 +1,16 @@
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+
 import App from './App';
-ReactDOM.render(<App />, document.querySelector('main'));
+import Reducer from '../store/reducer';
+
+const store = createStore(Reducer, applyMiddleware(ReduxThunk));
+
+const elem = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+ReactDOM.render(elem, document.querySelector('main'));
--- server/server.js
+++ server/server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const compression = require("compression");
 const path = require("path");
+const userInputLocations = require('./startingLocations.json');
 
 app.use(compression());
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
+
+app.get('/initial-user-locations', (req, res) => {
+    res.json(userInputLocations);
+});
 
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "..", "client", "index.html")));
 
