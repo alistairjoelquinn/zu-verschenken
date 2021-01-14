c2322132ca6ba5a2d13471ebfb1cfbc99028cf2e
Alistair Quinn <alistairjoelquinn@gmail.com>
2021-01-14 15:59:05 +0100

    component created for handling user input upon mouse click

---
+++ client/src/UserInputModal.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const UserInputStyles = styled.div`
+    position: absolute;
+    top: 30vh;
+    width: 60vw;
+`;
+
+const UserInputModal = () => {
+    return (
+        <UserInputStyles>
+            User Input Modal
+        </UserInputStyles>
+    );
+};
+
+export default UserInputModal;
