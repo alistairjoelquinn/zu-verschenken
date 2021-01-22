196db4c123ada6f415851c2d58a2551e1ee561d6
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-01-22 17:07:38 +0100

    slight info window style updates

--- client/src/ItemInfo.js
+++ client/src/ItemInfo.js
@@ -2,12 +2,27 @@ import { formatRelative } from 'date-fns';
 import styled from 'styled-components';
 
 const ItemInfoStyles = styled.div`
-    width: 20vw;
-    img {
-        display: block;
-        width: 100%;
-        height: 60%;
-        object-fit: cover;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    gap: 0.5rem;
+    font-family: RobotoMedium;
+    p {
+        font-family: Roboto;
+        margin: 0;
+    }
+    .image-cont {
+        width: 170px;
+        height: 170px;
+        overflow: hidden;
+        border: 0.5px solid black;
+        border-radius: 70%;
+        img {
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+        }
     }
 `;
 
@@ -17,7 +32,9 @@ const ItemInfo = ({ selectedGift, getDirections }) => {
             <h3>Zu Verschenken!</h3>
             <p>Seen {formatRelative(new Date(selectedGift.date), new Date())}</p>
             <p>Contains: {selectedGift.contents}</p>
-            <img src={selectedGift.url} alt={selectedGift.contents} />
+            <div className="image-cont">
+                <img src={selectedGift.url} alt={selectedGift.contents} />
+            </div>
             <button onClick={() => getDirections(parseFloat(selectedGift.lat), parseFloat(selectedGift.lng))}>
                 Directions
             </button>
