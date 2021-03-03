4fb786c37d25d69e6c5cabea6f0b591f25d0d336
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-03-03 16:35:26 +0100

    upload file size set

--- server/aws/upload.js
+++ server/aws/upload.js
@@ -2,13 +2,8 @@ const multer = require('multer');
 
 const multerOptions = {
     storage: multer.memoryStorage(),
-    fileFilter(req, file, next) {
-        const isImage = file.mimetype.startsWith('image/');
-        if (isImage) {
-            next(null, true);
-        } else {
-            next({ message: 'File type not supported' }, false);
-        }
+    limits: {
+        fileSize: 2097152
     }
 };
 
